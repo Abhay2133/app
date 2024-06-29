cd28d6051c51da19b26f2f1dbfec68049cb2b783
abhay2133 <abhaybishthestudent@gmail.com>
2024-06-29 10:06:42 +0530

    updated homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+// import Image from "next/image";
+import "./page.css";
+
+export default function Home() {
+  return (
+    <section className="hero">
+      <div className="hero-container">
+        <h1 className="hero-title">Full-Stack Dev | Mobile & Desktop Apps</h1>
+        <p className="hero-subheading">Mastery in frontend, backend, and system design for exceptional user experiences.</p>
+        <a href="#" className="hero-button">Explore My Portfolio</a>
+      </div>
+    </section>
+
+
+  );
+}
