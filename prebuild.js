f6a2a9fe18ef520baeb404d54b434154cb676b80
abhay2133 <abhaybishthestudent@gmail.com>
2024-07-02 22:59:09 +0530

    log content on build

--- prebuild.js
+++ prebuild.js
@@ -8,3 +8,5 @@ if(!process.env.DATABASE_URL && !process.env?.NODE_ENV?.startsWith("production")
     content += `DATABASE_URL=postgresql://abhay:abhay123@localhost:5432/apps\n`
 }
 writeFileSync(".env", content)
+
+console.log(content)
