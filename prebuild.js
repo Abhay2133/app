d046d23e5ae1812af1b3350777ed7634464f547f
abhay2133 <abhaybishthestudent@gmail.com>
2024-07-03 13:28:20 +0530

    removed NODE_ENV in build

--- prebuild.js
+++ prebuild.js
@@ -1,6 +1,6 @@
 const { writeFileSync } = require("fs");
 
-const url = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : "postgresql://abhay:abhay123@localhost:5432/apps"
+const url = process.env.DATABASE_URL || "postgresql://abhay:abhay123@localhost:5432/apps"
 
 let content = `DATABASE_URL="${url}"\n`
 writeFileSync(".env", content)
